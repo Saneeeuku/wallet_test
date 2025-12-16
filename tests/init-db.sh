5fb4e746e69e5dac73908fddd387913f15efe622
AlexKsky <sharkline942@gmail.com>
2025-12-16 14:08:39 +0300

    added gitattribures for sh scripts

--- tests/init-db.sh
+++
@@ -1,8 +0,0 @@
-# init-db.sh
-#!/bin/bash
-set -e
-
-psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
-    CREATE DATABASE test;
-    GRANT ALL PRIVILEGES ON DATABASE test TO "$POSTGRES_USER";
-EOSQL
